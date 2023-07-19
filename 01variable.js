d12c251613a3f5f82eb104838c099e6ffa5164e6
Kiirankale <kirankale431@gmail.com>
2023-07-19 20:39:45 +0530

    new program
    added info

---
+++ 01variable.js
@@ -0,0 +1,25 @@
+var myFullName;
+console.log( "Before Initialization :" + typeof myFullName);
+myFullName = "Kiran Kale";
+console.log( "After initialization :" + myFullName)
+myFullName = " Kiran Bj Kale";
+console.log( "After Update :" + myFullName)
+
+console.log("---------------------------------------------------------");
+var myLove = " JAVASCRIPT";
+console.log( "My First love is : "+ myLove);
+var name = "First name : Kiran";
+var lname = "Last name : Kale";
+var age = "Age:" +    28;
+var add = "Address  : Chatrapati sambhajiNagar";
+var pin = 442902;
+var gender = "GENDER : male";
+var marry = false;
+console.log( name);
+console.log( lname);
+console.log( age);
+console.log( add);
+console.log( "Pincode :" + pin);
+console.log( "is married :" + marry);
+console.log("--------------------------------------------------------- ------------");
+// Assignment 2//
---
+++ 02_typeOf.js
@@ -0,0 +1,25 @@
+var bankName;
+console.log( "Before Initialization :" + bankName);
+bankName = "SBI BANK";
+console.log( "After Initialization :" + bankName);
+console.log( "Type after Initialization : " + typeof bankName);
+console.log("-------------------------------------------------------------------------------------");
+var marks = "90%";
+console.log("Marks :" + marks);
+console.log( "Type of data type :" + typeof marks);
+console.log("-------------------------------------------------------------------------------------");
+var isWorking;
+console.log(" Before initialization value is :" + isWorking);
+console.log( " Before initialization data type is :" + typeof isWorking);
+isWorking = true;
+console.log(" After initialization value is :" + isWorking);
+console.log( " After initialization data type is :" + typeof isWorking);
+console.log("-------------------------------------------------------------------------------------");
+var num = " Value is :" + " One Hundred and Seven";
+console.log( num);
+console.log(" Type of value :" + typeof num);
+var newNum = 107;
+console.log(" Value is :" + newNum);
+console.log("Type of Value :" + typeof newNum);
+
+
